daac365daf4bdc8c8271b222e0f0f1dd247815c0
Kennedy-Anderson <kennedyandrade98@gmail.com>
2020-07-29 18:39:10 -0300

    Adicionando Spa

--- src/components/Menu/index.js
+++ src/components/Menu/index.js
@@ -1,17 +1,19 @@
 import React from 'react';
+import {Link} from 'react-router-dom';
 import Logo from '../../assents/img/Logo.png';
 import './Menu.css';
-import ButtonLink from './components/ButtonLink';
+import Button from '../Button';
+//import ButtonLink from './components/ButtonLink';
 
 function Menu(){
     return(
         <nav className="Menu" >
-            <a href="/">
+            <Link to="/">
                <img className="Logo" src= {Logo} alt="Logo do DevFlix"/>
-            </a>
-            <ButtonLink className="ButtonLink" href="/">
+            </Link>
+            <Button as={Link} className="ButtonLink" to="/cadastro/video">
                 Novo Video
-            </ButtonLink>
+            </Button>
 
         </nav>
 
--- src/index.js
+++ src/index.js
@@ -1,12 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
-
+import Home from './pages/Home';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import CadastroVideo from './pages/cadastro/Video/index.js'
+import CadastroCategoria from './pages/cadastro/Categoria';
+  
+  const Pagina404 = () => ( <div> Página 404</div>);
+  
+  ReactDOM.render(
+  
+    <BrowserRouter>
+      <Switch>
+        <Route path="/" component={Home} exact/>
+        <Route path="/cadastro/video" component={CadastroVideo}/>
+        <Route path="/cadastro/cadastro/categoria" component={CadastroCategoria}/>
+        <Route component={Pagina404}/>
+      </Switch>
+    </BrowserRouter>,
+    document.getElementById('root')
+  );
