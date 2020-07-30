73c5a4c5316a3d98bcc6e4bb718e531b2840a95c
Kennedy-Anderson <kennedyandrade98@gmail.com>
2020-07-30 13:59:10 -0300

    ESLint

--- src/pages/cadastro/Categoria/index.js
+++ src/pages/cadastro/Categoria/index.js
@@ -53,7 +53,7 @@ function CadastroCategoria(){
 
         <FormField 
            label="Descrição"
-           type="???"
+           type="textarea"
            name="descricao"
            value={values.descricao}
            onChange={handleChange}
